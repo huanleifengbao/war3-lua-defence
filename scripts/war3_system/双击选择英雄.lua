962e6800ca2c83283aec6debd248de66cb504f0d
幻雷 <952689065@qq.com>
2019-01-16 17:32:04 +0800

    选英雄好了.jpg

--- scripts/war3_system/双击选择英雄.lua
+++ scripts/war3_system/双击选择英雄.lua
@@ -1,15 +1,19 @@
-ac.game:event('单位-创建', function (_, unit)
-    print(unit:getOwner()._id,unit:getName())
-    if unit:getOwner()._id == 16 then
-        unit:event('玩家-选中单位', function (trg, player, unit)
-            print('ga')
-            print(player._id)
-            if not player:getHero() then
+local pick_mark = {}
+
+for i = 1, 6 do
+    ac.player(i):event('玩家-选中单位', function (trg, player, unit)
+        if unit:getOwner()._id == 16 and not player:getHero() then
+            if pick_mark[i] == unit then
                 trg:remove()
                 unit:setOwner(player, true)
                 player:addHero(unit)
-                unit:blink({7044, -8792})
+                unit:blink(ac.point(7044, -8792))
+            else
+                pick_mark[i] = unit
+                ac.wait(400, function()
+                    pick_mark[i] = false
+                end)
             end
-        end)
-    end
-end)
+        end
+    end)
+end
