ea5c566e2d41d165d477316060b305c1bd0f5995
幻雷 <952689065@qq.com>
2019-01-16 14:48:55 +0800

    暂存

--- scripts/war3_system/双击选择英雄.lua
+++ scripts/war3_system/双击选择英雄.lua
@@ -1,7 +1,8 @@
-ac.event('单位-创建', function (trg, unit)
-    print(unit:getOwner(),unit:getname())
+ac.game:event('单位-创建', function (_, unit)
+    print(unit:getOwner()._id,unit:getName())
     if unit:getOwner() then
         unit:event('玩家-选中单位', function (trg, player, unit)
+            print('gaga')
             unit:remove()
         end)
         unit:event('玩家-取消选中', function (trg, player, unit)
