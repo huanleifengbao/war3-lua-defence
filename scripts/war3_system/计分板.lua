0824571f48bb14e6288495bd9458182e6e6ca895
幻雷 <952689065@qq.com>
2019-02-14 16:32:37 +0800

    暂存

--- scripts/war3_system/计分板.lua
+++ scripts/war3_system/计分板.lua
@@ -14,15 +14,15 @@ local player_colour = {
     [12] = '|c44400000',
 }
 
+--威望
 local title = {
-    [1] = '|cffffffcc弟弟|r',
-    [2] = '|cff505050宵暗的妖怪',
-    [3] = '|cff00ffff⑨',
-    [4] = '|cff00aa00华人小姑娘',
-    [5] = '|cffff00ff姆Q~★',
-    [6] = '|cffccffffPAD',
-    [7] = '|cffff0000「永远的红月」',
-    [8] = '|cffffcc00「恶魔之妹」',
+    [1] = '|cffffffcc初入江湖|r',
+    [2] = '|cff505050武林菜鸟',
+    [3] = '|cff00ffff声名鹊起',
+    [4] = '|cff00aa00武林高手',
+    [5] = '|cffff00ff一代宗师',
+    [6] = '|cffccffff武林至尊',
+    [7] = '|cffff0000独孤求败',
     [100] = '|cffffffff最多七个汉字哦',
 }
 local title_icon = {
@@ -33,41 +33,68 @@ local title_icon = {
     [5] = [[ReplaceableTextures\CommandButtons\BTNNecromancerAdept.blp]],
     [6] = [[ReplaceableTextures\CommandButtons\BTNArcaniteArmor.blp]],
     [7] = [[ReplaceableTextures\CommandButtons\BTNSearingArrows.blp]],
-    [8] = [[ReplaceableTextures\CommandButtons\BTNVampiricAura.blp]],
 }
 local title_level = {
     [1] = 0,
-    [2] = 5,
-    [3] = 9,
-    [4] = 20,
-    [5] = 30,
-    [6] = 45,
-    [7] = 60,
-    [8] = 100,
+    [2] = 10,
+    [3] = 2000,
+    [4] = 3000,
+    [5] = 4000,
+    [6] = 5000,
+    [7] = 6000,
 }
-local player_title = {}
-for i = 1, 6 do
-    player_title[i] = 1
-end
+
+--觉醒
+local title2 = {
+    [1] = '|cffffffcc横扫千军|r',
+    [2] = '|cff505050铜墙铁壁',
+    [3] = '|cff00ffff妙手回春',
+    [4] = '|cff00aa00势如破竹',
+    [5] = '|cffff00ff拨云见日',
+    [6] = '|cffccffff森罗万象',
+    [7] = '|cffff0000足智多谋',
+    [8] = '|cffff0000不动如山',
+    [9] = '|cffff0000天神下凡',
+    [10] = '|cffff0000策划案没写',
+    [100] = '|cffffffff最多七个汉字哦',
+}
+local title2_icon = {
+    [1] = [[ReplaceableTextures\CommandButtons\BTNPenguin.blp]],
+    [2] = [[ReplaceableTextures\CommandButtons\BTNDispelMagic.blp]],
+    [3] = [[ReplaceableTextures\CommandButtons\BTNIceShard.blp]],
+    [4] = [[ReplaceableTextures\CommandButtons\BTNGauntletsOfOgrePower.blp]],
+    [5] = [[ReplaceableTextures\CommandButtons\BTNNecromancerAdept.blp]],
+    [6] = [[ReplaceableTextures\CommandButtons\BTNArcaniteArmor.blp]],
+    [7] = [[ReplaceableTextures\CommandButtons\BTNSearingArrows.blp]],
+    [8] = [[ReplaceableTextures\CommandButtons\BTNSearingArrows.blp]],
+    [9] = [[ReplaceableTextures\CommandButtons\BTNSearingArrows.blp]],
+    [10] = [[ReplaceableTextures\CommandButtons\BTNSearingArrows.blp]],
+}
 
 ac.wait(0, function()
-    local board = ac.game:board(7, 6, '初始化中')
+    local board = ac.game:board(7, 9, '初始化中')
     ac.wait(1, function()
         board:show()
         board:style(true, false)
         board[1][1]:text('基地')
-        board[1][2]:text('英雄')
-        board[1][3]:text('Lv')
-        board[1][4]:text('杀敌')
-        board[1][5]:text('翻车')
-        board[1][6]:text('－－－－－称号－－－－－')
-        for i = 1, 7 do
+        board[1][2]:text('战力')
+        board[1][3]:text('抗性')
+        board[1][4]:text('－－－－－威望－－－－－')
+        board[1][5]:text('－－－－－觉醒－－－－－')
+        board[1][6]:text('战魂')
+        board[1][7]:text('－坐骑－')
+        board[1][8]:text('杀敌')
+        board[1][9]:text('木材')
+        for i = 1, 9 do
             board[i][1]:width(0.07)
-            board[i][2]:width(0.05)
+            board[i][2]:width(0.02)
             board[i][3]:width(0.02)
-            board[i][4]:width(0.02)
-            board[i][5]:width(0.025)
-            board[i][6]:width(0.06)
+            board[i][4]:width(0.06)
+            board[i][5]:width(0.06)
+            board[i][6]:width(0.02)
+            board[i][7]:width(0.035)
+            board[i][8]:width(0.02)
+            board[i][9]:width(0.03)
             if i ~= 1 then
                 local player = ac.player(i-1)
                 if player and player:controller() == '用户' and player:gameState() == '在线' then
@@ -80,20 +107,25 @@ ac.wait(0, function()
     ac.game:event('地图-选择英雄', function (_, unit, player)
         local id = player:id()
         if unit then
-            board[id+1][2]:style(true, true)
-            board[id+1][2]:icon(unit:slk('art'))
-            board[id+1][2]:text(unit:getName())
-            board[id+1][3]:text('1')
-            board[id+1][4]:text('0')
-            board[id+1][5]:text('|cffff0000没死过|r')
-            board[id+1][6]:style(true, true)
-            board[id+1][6]:icon(title_icon[player_title[id]])
-            board[id+1][6]:text(title[player_title[id]])
+            board[id+1][1]:style(true, true)
+            board[id+1][1]:icon(unit:slk('art'))
+            board[id+1][2]:text(unit:get('战力'))
+            board[id+1][3]:text(unit:get('抗性'))
+            board[id+1][4]:style(true, true)
+            board[id+1][4]:icon(title_icon[unit:get('威望')])
+            board[id+1][4]:text(title[unit:get('威望')])
+            board[id+1][5]:style(true, true)
+            board[id+1][5]:icon(title2_icon[unit:get('觉醒')])
+            board[id+1][5]:text(title2[unit:get('觉醒')])
+            board[id+1][6]:text('0')
+            board[id+1][7]:text('光脚QAQ')
+            board[id+1][8]:text('0')
+            board[id+1][9]:text('0')
         end
     end)
 
     --等级
-    ac.game:event('地图-选择英雄', function (_, unit, player)
+    --[[ac.game:event('地图-选择英雄', function (_, unit, player)
         unit:event('单位-升级', function (trg, unit)
             local id = player:id()
             board[id+1][3]:text(unit:level())
@@ -102,10 +134,10 @@ ac.wait(0, function()
             local id = player:id()
             board[id+1][3]:text(unit:level())
         end)
-    end)
+    end)]]
 
     --死亡
-    local hero_dead = {}
+    --[[local hero_dead = {}
     for i = 1, 6 do
         hero_dead[i] = 0
     end
@@ -115,7 +147,7 @@ ac.wait(0, function()
             hero_dead[id] = hero_dead[id] + 1
             board[id+1][5]:text(hero_dead[id])
         end
-    end)
+    end)]]
 
     --击杀
     local hero_kill = {}
@@ -126,13 +158,13 @@ ac.wait(0, function()
         local id = player:id()
         if unit then
             hero_kill[id] = hero_kill[id] + 1
-            board[id+1][4]:text(hero_kill[id])
+            board[id+1][8]:text(hero_kill[id])
             --称号
-            if player_title[id] < #title_level and hero_kill[id] >= title_level[player_title[id] + 1] then
-                player_title[id] = player_title[id] + 1
-                board[id+1][6]:icon(title_icon[player_title[id]])
-                board[id+1][6]:text(title[player_title[id]])
-                print(unit:getName()..'成为了'..title[player_title[id]])
+            if unit:get('威望') < #title_level and hero_kill[id] >= title_level[unit:get('威望') + 1] then
+                unit:add('威望', 1)
+                board[id+1][4]:icon(title_icon[unit:get('威望')])
+                board[id+1][4]:text(title[unit:get('威望')])
+                print(unit:getName()..'成为了'..title[unit:get('威望')])
             end
         end
     end)
@@ -156,10 +188,22 @@ ac.wait(0, function()
         elseif num == 4 then
             game_mod_str = '|cffff00ffLunatic|r'
         end
-        game_mod_str = '|cffffcc00难度: |r'..game_mod_str
+        game_mod_str = '|cffffcc00当前难度: |r'..game_mod_str
     end)
 
     ac.loop(1, function()
+        --修改玩家属性
+        for i = 1, 6 do
+            local player = ac.player(i)
+            if player and player:controller() == '用户' then
+                local unit = player:gethero()
+                if unit then
+                    board[i+1][2]:text(unit:get('战力'))
+                    board[i+1][3]:text(unit:get('抗性'))
+                end
+            end
+        end
+        
         --修改标题
         game_time_str = ''
         if game_time[1] >= 59 then
@@ -176,16 +220,16 @@ ac.wait(0, function()
         if game_time[3] < 10 then
             game_time_str = game_time_str..'0'
         end
-        game_time_str = game_time_str..game_time[3]..':'
+        game_time_str = game_time_str..game_time[3]..'时'
         if game_time[2] < 10 then
             game_time_str = game_time_str..'0'
         end
-        game_time_str = game_time_str..game_time[2]..':'
+        game_time_str = game_time_str..game_time[2]..'分'
         if game_time[1] < 10 then
             game_time_str = game_time_str..'0'
         end
-        game_time_str = game_time_str..game_time[1]
-        board:title('|cffffff00〓传奇三国积分版〓  |r'..game_mod_str..'    '..game_time_str)
+        game_time_str = game_time_str..game_time[1]..'秒'
+        board:title('|cffffff00〓传奇三国〓             玩家英雄属性     |r'..game_mod_str..'    '..'游戏时间:'..game_time_str)
 
         --显示基地血量
         local base_str = '基地[|cffffcc00'
