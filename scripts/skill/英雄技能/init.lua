3b7589108e26f4e36feab945641dd33823d0c462
张天正02 <zhangtianzheng02@7fgame.net>
2019-02-19 17:03:28 +0800

    暂存

---
+++ scripts/skill/英雄技能/init.lua
@@ -0,0 +1,1 @@
+require 'skill.英雄技能.天楼闪薙斩'
--- scripts/war3_system/计分板.lua
+++ scripts/war3_system/计分板.lua
@@ -109,8 +109,9 @@ ac.wait(0, function()
         local id = player:id()
         if unit then
             --开挂
-            unit:addSkill('赤兔', '技能', 1)
+            unit:addSkill('天楼闪薙斩', '技能', 1)
             unit:addSkill('三国无双', '技能', 2)
+            unit:addSkill('赤兔', '技能', 3)
             --初始化积分相关属性
             unit:set('威望等级', 1)
             unit:set('觉醒等级', 1)
