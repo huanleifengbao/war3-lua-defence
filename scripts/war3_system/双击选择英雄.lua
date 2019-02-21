34469f69bdced465815fac4e4145bfd542e38151
张天正02 <zhangtianzheng02@7fgame.net>
2019-02-21 18:21:43 +0800

    1级专属

--- scripts/war3_system/双击选择英雄.lua
+++ scripts/war3_system/双击选择英雄.lua
@@ -1,5 +1,19 @@
 local pick_mark = {}
 
+local Aghanim = {
+    ['圣骑士'] = '真黄龙剑',
+    ['关羽'] = '黄龙偃月刀',
+    ['张飞'] = '破军蛇矛',
+    ['黄忠'] = '破邪旋风斩',
+    ['诸葛亮'] = '朱雀羽扇',
+    ['吕布'] = '无双方天戟',
+    ['赵云'] = '豪龙胆',
+    ['司马懿'] = '穷奇羽扇',
+    ['星彩'] = '煌天',
+    ['貂蝉'] = '金丽玉霞',
+    ['孙尚香'] = '日月乾坤剑',
+}
+
 for i = 1, 6 do
     ac.player(i):event('玩家-选中单位', function (trg, player, unit)
         if unit:getOwner():id() == 16 and not player:getHero() then
@@ -10,6 +24,11 @@ for i = 1, 6 do
                 local start_p = ac.point(7044, -8792)
                 unit:blink(start_p)
                 unit:bagSize(6)
+                local item_name = Aghanim[unit:getName()]
+                if item_name then
+                    local item = unit:createItem(item_name..'-1')
+                    unit:userData('专属', item)
+                end
                 player:moveCamera(start_p, 0.2)
                 ac.game:eventNotify('地图-选择英雄', unit, player)
             else
