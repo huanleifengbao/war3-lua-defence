7893de5dfbe0c8a90c915b8ca139b71ef0f0bf34
张天正02 <zhangtianzheng02@7fgame.net>
2019-02-26 14:46:28 +0800

    暂存

--- scripts/war3_system/可刷新野外boss.lua
+++ scripts/war3_system/可刷新野外boss.lua
@@ -9,10 +9,10 @@ local tbl = {
 for i = 1, #tbl do
     local boss = ac.player(11):createUnit(tbl[i][1], tbl[i][2], 270)
     local trg = boss:event('单位-死亡', function (_, _, killer)
-        print(boss:getName()..'又死了 没人性')
+        print(boss:getName()..'又死了 没人性', boss, tbl[i][2])
         local timer = ac.wait(tbl[i][3], function()
-            boss:reborn(tbl[i][2], true)
-            print(boss:getName()..'复活了')
+            print(boss:reborn(tbl[i][2], true))
+            print(boss:getName()..'复活了', boss, tbl[i][2])
         end)
     end)
 end
