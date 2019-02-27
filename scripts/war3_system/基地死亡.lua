2e1759ee7accb81e18f5887fe9942ec26e3babda
张天正02 <zhangtianzheng02@7fgame.net>
2019-02-27 14:39:37 +0800

    ?

--- scripts/war3_system/基地死亡.lua
+++ scripts/war3_system/基地死亡.lua
@@ -2,7 +2,18 @@ ac.game:event('单位-创建', function (_, unit)
     if unit:getName() == '基地' then
         sg.base = unit
         unit:event('单位-死亡', function (trg, unit)
-            print('家都boom了你不会自己退?rua')
+            for i = 1, 6 do
+                ac.player(i):message('|cffffff00家都|cffff0000boom|cffffff00了你不会自己退?|r', 30)
+                ac.wait(2, function()
+                    ac.player(i):message('|cffff0000哼!|r', 30)
+                    ac.wait(1, function()
+                        ac.player(i):message('|cffffff00免为其难让你|cff00ff00继续玩|cffffff00一会吧|r', 30)
+                        ac.wait(2, function()
+                            ac.player(i):message('|cffffff00下次给我好好地守住啊!|cffff0000baka!|r', 30)
+                        end)
+                    end)
+                end)
+            end
         end)
     end
 end)
