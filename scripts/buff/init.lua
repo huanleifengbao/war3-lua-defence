8db0c9fedd76548b96093b2a24bfd33de9d3feff
朱哲靖02 <zhuzhejing02@7fgame.net>
2019-02-27 14:40:33 +0800

    加个Buff

---
+++ scripts/buff/init.lua
@@ -0,0 +1,1 @@
+require 'buff.通用'
--- scripts/main.lua
+++ scripts/main.lua
@@ -4,6 +4,7 @@ jass = require 'jass.common'
 require 'game'
 require 'skill'
 require 'item'
+require 'buff'
 require 'war3_system'
 jass.FogEnable(false)
 jass.FogMaskEnable(false)
