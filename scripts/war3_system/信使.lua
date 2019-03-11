a8f9dce310c6fd2aff0ace3d468f210576b07cec
朱哲靖02 <zhuzhejing02@7fgame.net>
2019-03-11 16:37:58 +0800

    切换背包改了

--- scripts/war3_system/信使.lua
+++ scripts/war3_system/信使.lua
@@ -73,25 +73,11 @@ function mt:onAdd()
 	local hero = self:getOwner()
 	local bag = {}
 	for i = 1,page do
-		--local u = hero:createUnit('切换背包',ac.point(0,0),0)
-		--u:bagSize(6)
 		bag[i] = {}
 	end
 	self.max_page = page
 	now_page[hero] = 1
 	self.bag = bag
-	--local player = hero:getOwner()
-	--function player:get_chick()
-	--	local tbl = {hero}
-	--	for i = 1,#bag do
-	--		table.insert(tbl,bag[i])
-	--	end
-	--	return tbl
-	--end
-	--local hero = player:getHero()
-	--function hero:get_chick()
-	--	return player:getMesgerbag()
-	--end
 end
 
 function mt:onCastShot()
@@ -101,8 +87,9 @@ function mt:onCastShot()
 	local now_bag = now_page[hero]
 	for item in hero:eachItem() do
 		local slot = item:getSlot()
-		--item:give(bag[now_page[hero]],slot)
 		bag[now_bag][slot] = item
+		item:blink(ac.point(0,0))
+		item:hide()
 	end
 	if now_bag < max_page then
 		now_page[hero] = now_bag + 1
@@ -112,10 +99,7 @@ function mt:onCastShot()
 	now_bag = now_page[hero]
 	for slot,item in pairs(bag[now_bag]) do
 		item:give(hero,slot)
+		item:show()
 	end
-	--for item in bag[now_page[hero]]:eachItem() do
-	--	local slot = item:getSlot()
-	--	item:give(hero,slot)
-	--end
 	hero:getOwner():message('当前背包：' .. now_page[hero] .. '/' .. max_page,3)
 end
